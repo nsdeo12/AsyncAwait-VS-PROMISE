fb777ede3e87df225563e8221393e44c95da478d
root <root@localhost.localdomain>
2018-03-18 02:28:55 -0400

    merged from dev branch to master

--- 3ismomhappy.js
+++ 3ismomhappy.js
@@ -1,3 +1,16 @@
+//A Promise in short:
+/*"Imagine you are a kid. 
+Your mom promises you that she'll get you
+a new phone next week."*/
+
+/**
+ * 
+ * That is a promise. A promise has 3 states. They are:
+ * Promise is pending: You don't know if you will get that phone until next week.
+ * Promise is resolved: Your mom really buy you a brand new phone.
+ * Promise is rejected: You don't get a new phone because your mom is not happy. 
+ */
+
 var isMomHappy = false;
 
 // Promise
@@ -23,13 +36,28 @@ var askMom = function () {
         .then(function (fulfilled) {
             // yay, you got a new phone
             console.log(fulfilled);
-         // output: { brand: 'Samsung', color: 'black' }
+            // output: { brand: 'Samsung', color: 'black' }
         })
         .catch(function (error) {
             // oops, mom don't buy it
             console.log(error.message);
-         // output: 'mom is not happy'
+            // output: 'mom is not happy'
         });
 };
 
 askMom();
+/**
+ * We have a function call askMom. 
+ * In this function, we will consume our promise willIGetNewPhone.
+ * We want to take some action once the promise is resolved or rejected,
+ *  we use .then and .catch to handle our action.
+ * In our example, we have function(fulfilled) { ... } in .then. 
+ * What is the value of fulfilled? The fulfilled value is exactly 
+ * the value you pass in your promise resolve(your_success_value).
+ * Therefore, it will be phone in our case.
+ * We have function(error){ ... } in .catch. 
+ * What is the value of error? 
+ * As you can guess, the error value is exactly 
+ * the value you pass in your promise reject(your_fail_value). 
+ * Therefore, it will be reason in our case.
+ */
---
+++ 4chainingPromise.js
@@ -0,0 +1,2 @@
+//Let's say, you, the kid, promise your friend that you will show them the new phone when your mom buy you one.
+
